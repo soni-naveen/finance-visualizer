28bc1c3231bbb0cd2d51e991b422566b25f609ad
Naveen Soni <naveennsonii@gmail.com>
2025-07-12 00:46:43 +0530

    style: footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-gray-400 py-5 text-center text-sm text-muted-foreground">
+    <footer className="w-full border-t border-gray-400 py-5 text-center text-xs xs:text-sm text-muted-foreground">
       © {new Date().getFullYear()} Finance Visualizer. Developed by{" "}
       <a
         target="_blank"
